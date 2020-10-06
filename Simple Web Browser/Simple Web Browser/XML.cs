6fb6c267171bc1c6e676acff1a53b4819566f5c5
Lewis <Lewiswilson1497@gmail.com>
2020-10-06 22:06:58 +0100

    Refactored XML class

--- Simple Web Browser/Simple Web Browser/XML.cs
+++ Simple Web Browser/Simple Web Browser/XML.cs
@@ -5,41 +5,33 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Xml;
-using System.Xml.Linq;
+
 
 namespace Simple_Web_Browser
 {
     class XML
     {
-        // XDocument myxml;
-        XmlDocument doc = new XmlDocument();
-        
+        private XmlDocument doc;
+        private string URI;
 
         public XML(string filename)
         {
-            // myxml = XDocument.Load(Properties.Resources.Homepage);
+            doc = new XmlDocument();
             doc.Load(Properties.Resources.Homepage);
+
+            // if(filename == Homepage.xml) { ...
         }
 
-        /// <summary>
-        /// returns the home page from the Homepage.xml file
-        /// </summary>
         public string getHomePageURI()
         {
-            // return myxml.Root.Value;
-            XmlNode nodeList = doc.SelectSingleNode("//link");
-            string link = nodeList.InnerText;
-            System.Console.WriteLine(link);
+            XmlNode node = doc.SelectSingleNode("//link");
+            URI = node.InnerText;
          
-            return link;
+            return URI;
 
         }
 
-        /// <summary>
-        /// Sets the home page in Homepage.xml
-        /// </summary>
-        /// <param name="URI"> the value we are setting in the XML document </param>
-        public static void writeHomePage(String URI)
+        public static void setHomePageURI(String URI)
         {
             
         }
