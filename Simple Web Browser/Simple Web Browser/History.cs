a8d6e8dc825460012cb73271ca2d2ec10182e8a3
Lewis <Lewiswilson1497@gmail.com>
2020-10-10 21:30:19 +0100

    History

---
+++ Simple Web Browser/Simple Web Browser/History.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Simple_Web_Browser
+{
+    public class History
+    {
+
+        // History stuff
+
+        public static List<string> item = new List<string>();
+        static public int index = 0;
+        static string url = "";
+
+        public bool CanStepBack = item.ElementAtOrDefault(index - 1) != null ? true : false;
+        public bool CanStepForward = item.ElementAtOrDefault(index + 1) != null ? true : false;
+        public bool addedURL;
+
+        public History(string initalURI)
+        {
+
+            item.Add(initalURI);
+            url = initalURI;
+
+            /*a
+            foreach (string x in item)
+            {
+                Console.WriteLine(x);
+            }
+            */
+        }
+        
+
+        public string getNextPage()
+        {
+
+            index++;
+            Console.WriteLine("Returning {0} at index {1}",item[index], index);
+            return item[index];
+        }
+
+        
+        public string getPreviousPage()
+        { 
+            index--;
+            Console.WriteLine("Returning {0} at index {1}", item[index], index);
+            return item[index];
+        }
+
+        public void addToHistory(string URL)
+        {
+            // don't add an element to history if the same URL is trying to be added again 
+            if(item.ElementAt(item.Count -1) != URL)
+            {
+                index++;
+                item.Add(URL);
+                addedURL = true;
+                  
+            } else
+            {
+                addedURL = false;
+            }
+
+            
+        }
+
+        
+    }
+}
