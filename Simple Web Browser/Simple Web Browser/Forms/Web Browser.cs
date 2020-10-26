d68d4a6a41ee4381b680074f7cc3d3fb8c949667
Lewis <Lewiswilson1497@gmail.com>
2020-10-26 11:36:09 +0000

    Removed imports

--- Simple Web Browser/Simple Web Browser/Forms/Web Browser.cs
+++ Simple Web Browser/Simple Web Browser/Forms/Web Browser.cs
@@ -1,14 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
-using System.Net.Http;
-using System.Text.RegularExpressions;
+
 
 namespace Simple_Web_Browser
 {
